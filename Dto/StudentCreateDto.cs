616e3eb10e6d4c7078881adc884ee8ecf183b18a
Ritika Arora <ritikaaroramcageu@gmail.com>
2026-05-08 16:37:32 +0530

    changed angular app cors

--- Dto/StudentCreateDto.cs
+++ Dto/StudentCreateDto.cs
@@ -1,8 +1,12 @@
-﻿namespace StudentCrudAppWithEFCoreCodeFirst.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCrudAppWithEFCoreCodeFirst.Dto
 {
     public class StudentCreateDto
     {
+        [Required]
         public string? StudentName { get; set; }
+        [Range(5,100)]
         public int StudentAge { get; set; }
         public int StudentMarks { get; set; }
 
